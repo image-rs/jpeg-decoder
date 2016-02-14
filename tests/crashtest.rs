e1624ad0a44da9ca0fa52832164ae4ab48c64842
Ulf Nilsson <kaksmet@gmail.com>
2016-02-14 11:51:26 +0100

    Share test_files between crashtest.rs and reftest.rs

--- tests/crashtest.rs
+++ tests/crashtest.rs
@@ -2,53 +2,18 @@ extern crate jpeg_decoder as jpeg;
 extern crate walkdir;
 
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::{Path, PathBuf};
-use walkdir::WalkDir;
+use std::io::BufReader;
+use std::path::Path;
+
+mod common;
 
 #[test]
 fn crashtest() {
-    let files = test_files(&Path::new("tests").join("crashtest"));
+    let files = common::test_files(&Path::new("tests").join("crashtest"));
 
     for path in &files {
         let file = File::open(path).unwrap();
         let mut decoder = jpeg::Decoder::new(BufReader::new(file));
         let _ = decoder.decode();
     }
-}
-
-fn test_files(test_dir: &Path) -> Vec<PathBuf> {
-    let mut test_files = vec![];
-
-    for entry in WalkDir::new(&test_dir) {
-        let entry = entry.unwrap();
-        let path = entry.path();
-
-        if let Some(extension) = path.extension() {
-            if path.is_file() && (extension == "jpg" || extension == "jpeg") {
-                test_files.push(path.to_owned());
-            }
-        }
-    }
-
-    if let Ok(file) = File::open(test_dir.join("disabled.list")) {
-        for line in BufReader::new(file).lines() {
-            let line = line.unwrap();
-
-            if line.is_empty() || line.starts_with("#") {
-                continue;
-            }
-
-            let path = test_dir.join(Path::new(&line));
-
-            if !test_files.contains(&path) {
-                panic!("The file {:?} specified in {:?} could not be found among the files being tested", line, test_dir.join("disabled.txt"));
-            }
-
-            let position = test_files.iter().position(|p| p == &path).unwrap();
-            test_files.remove(position);
-        }
-    }
-
-    test_files
 }
