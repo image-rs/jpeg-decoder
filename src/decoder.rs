344a04ef997f6def53ecca9dd6ae97e6e9e60e04
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-12-09 22:09:11 +0100

    Optimize data copy in compute_image
    Data was copied byte by byte in two nested loops.
    This changes the code to a single loop, using memcpy to copy one line of data at a time.
    
    I observed a 2% performance improvement on the "decode 512x512 image" benchmark on a 2018 Macbook Pro.

--- src/decoder.rs
+++ src/decoder.rs
@@ -788,9 +788,11 @@ fn compute_image(components: &[Component],
         let line_stride = component.block_size.width as usize * component.dct_scale;
 
         for y in 0 .. height {
-            for x in 0 .. width {
-                buffer[y * width + x] = data[0][y * line_stride + x];
-            }
+            let destination_idx = y * width;
+            let source_idx = y * line_stride;
+            let destination = &mut buffer[destination_idx..destination_idx + width];
+            let source = &data[0][source_idx..source_idx + width];
+            destination.copy_from_slice(source);
         }
 
         Ok(buffer)
