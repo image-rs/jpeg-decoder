6e17452292493fcada321521391a33e324593427
Andreas Molzer <andreas.molzer@gmx.de>
2022-02-25 22:55:56 +0100

    Add test for multi-threaded usage with rayon

---
+++ tests/rayon-2.rs
@@ -0,0 +1,23 @@
+//! Must be a separate test because it modifies the _global_ rayon pool.
+use std::{fs::File, path::Path};
+use jpeg_decoder::Decoder;
+
+#[test]
+fn decoding_in_global_pool() {
+    let path = Path::new("tests/reftest/images/progressive3.jpg");
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(2)
+        .build_global()
+        .unwrap();
+
+    let _: Vec<_> = (0..1024)
+        .map(|_| {
+            let path = path.clone();
+            std::thread::spawn(move || {
+                let mut decoder = Decoder::new(File::open(&path).unwrap());
+                let _ = decoder.decode().unwrap();
+            });
+        }).collect();
+}
+
